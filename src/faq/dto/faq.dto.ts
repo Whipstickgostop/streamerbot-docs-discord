df77523b709ee756e7001a5d234dd227828c205e
Whipstickgostop <8366326+Whipstickgostop@users.noreply.github.com>
2024-02-24 13:24:32 -0500

    update faq payload

--- src/faq/dto/faq.dto.ts
+++ src/faq/dto/faq.dto.ts
@@ -1,12 +1,21 @@
 import { StringOption } from 'necord';
 
 export type FaqItem = {
+  _id: string;
   _path: string;
+  _type: string;
+  _source: string;
+  _file: string;
+  _extension: string;
+  _draft: boolean;
+  _partial: boolean;
+  _locale: string;
   title: string;
   description: string;
   content: string;
   images?: string[];
-}
+  youtubeId?: string;
+};
 
 export class FaqCommandDto {
   @StringOption({
